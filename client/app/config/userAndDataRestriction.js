4e4e379a02e1dbf1217a6cebb1660c56718eb386
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-04-14 14:57:37 +0530

    Add config file

---
+++ client/app/config/userAndDataRestriction.js
@@ -0,0 +1,7 @@
+// EXCLUSIVE
+export const MIN_TIME_DISABLED = 7;
+export const MAX_TIME_DISABLED = 19;
+
+export const RESTRICTED_DATA_SOURCES = ["ReplicaEC6"];
+
+export const RESTRICTED_HOURS = [7, 8, 9, 10, 11];
